2753310e23b8c5535336e6c988d02e7bd53e1319
Bukta Attila <attilabukta96@gmail.com>
2018-03-18 03:08:40 +0100

    Creating all the attributes which we will sort by

--- main.py
+++ main.py
@@ -9,6 +9,19 @@ class Attribute:
         self.maximum = ma
 
 con = None
+ratecode = Attribute('rate_code', 0, 6)
+passenger = Attribute('passenger_count', 0, 6)
+triptime = Attribute('trip_time_in_secs', 0, 10320)
+distance = Attribute('trip_distance', 0, 97.3)
+pickuplong = Attribute('pickup_longitude', -735.3, 0.0)
+pickuplat = Attribute('pickup_latitude', -3127.7, 3210.4)
+dropofflong = Attribute('dropoff_longitude', -2134.8, 1347.5)
+dropofflat = Attribute('dropoff_latitude', -0.6, 898.3)
+fare = Attribute('fare_amount', 2.5, 300.0)
+surcharge = Attribute('surcharge', 0.0, 3.0)
+tip = Attribute('tip_amount', 0.0, 165.0)
+toll = Attribute('tolls_amount', 0.0, 20.0)
+total = Attribute('total_amount', 2.5, 370.5)
 try:
 
     con = psycopg2.connect(database='taxi', user='postgres', password='postgres')

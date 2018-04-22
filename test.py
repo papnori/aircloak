8250e93b292202ef56bd706b41abe0c7ad1a8810
papnori <nori.753@gmail.com>
2018-04-22 22:26:30 +0200

    xsx

--- test.py
+++ test.py
@@ -1,107 +1,46 @@
-import psycopg2
-import sys
-import math
-import random
-import numpy
-import scipy
+from __future__ import print_function
+from ortools.linear_solver import pywraplp
 
-class Attribute:
+def main():
+  # Instantiate a Glop solver, naming it LinearExample.
+  solver = pywraplp.Solver('LinearExample',
+                           pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
 
-    def __init__(self, name, mi, ma):
-        self.name = name
-        self.minimum = mi
-        self.maximum = ma
+# Create the two variables and let them take on any value.
+  x = solver.NumVar(-solver.infinity(), solver.infinity(), 'x')
+  y = solver.NumVar(-solver.infinity(), solver.infinity(), 'y')
 
-#creating attributes
-ratecode = Attribute('rate_code', 0, 6)
-passenger = Attribute('passenger_count', 0, 6)
-triptime = Attribute('trip_time_in_secs', 0, 10320)
-distance = Attribute('trip_distance', 0, 97.3)
-pickuplong = Attribute('pickup_longitude', -735.3, 0.0)
-pickuplat = Attribute('pickup_latitude', -3127.7, 3210.4)
-dropofflong = Attribute('dropoff_longitude', -2134.8, 1347.5)
-dropofflat = Attribute('dropoff_latitude', -0.6, 898.3)
-fare = Attribute('fare_amount', 2.5, 300.0)
-surcharge = Attribute('surcharge', 0.0, 3.0)
-tip = Attribute('tip_amount', 0.0, 165.0)
-toll = Attribute('tolls_amount', 0.0, 20.0)
-total = Attribute('total_amount', 2.5, 370.5)
-attributes = [ratecode, passenger, triptime, distance, pickuplong, pickuplat, dropofflong, dropofflat, fare, surcharge, tip, toll, total]
+  # Constraint 1: x + 2y <= 14.
+  constraint1 = solver.Constraint(-solver.infinity(), 14)
+  constraint1.SetCoefficient(x, 1)
+  constraint1.SetCoefficient(y, 2)
 
+  # Constraint 2: 3x - y >= 0.
+  constraint2 = solver.Constraint(0, solver.infinity())
+  constraint2.SetCoefficient(x, 3)
+  constraint2.SetCoefficient(y, -1)
 
-rownr=100
-#rownr = 440257
-matrixrownr = rownr * pow(math.log(rownr), 2)
-matrix = []
-vector = []
-attributenrvector = []
+  # Constraint 3: x - y <= 2.
+  constraint3 = solver.Constraint(-solver.infinity(), 2)
+  constraint3.SetCoefficient(x, 1)
+  constraint3.SetCoefficient(y, -1)
 
+  # Objective function: 3x + 4y.
+  objective = solver.Objective()
+  objective.SetCoefficient(x, 3)
+  objective.SetCoefficient(y, 4)
+  objective.SetMaximization()
 
-#querrying
-con = None
-try:
-
-    con = psycopg2.connect(database='taxi', user='postgres', password='admin')
-    cur = con.cursor()
-
-    # cp builder loop
-    for i in range(int(matrixrownr)):
-        # random 5-15% (1-2)
-        attributenr = random.randint(1, 3)
-        # filling attributenrvector
-        attributenrvector.append(attributenr)
-        # choosing which attributes to querry
-        randomom = random.randint(1, 14)
-        firstattribute = attributes[randomom]
-        if attributenr == 2:
-            secondattribute = attributes[random.randint(1, 14)]
-            while secondattribute == firstattribute:
-                secondattribute = attributes[random.randint(1, 14)]
-        # normal distribution min max
-        firstattributecurrentmin = numpy.random.normal((firstattribute.maximum - firstattribute.minimum) / 2, 1)
-        firstattributecurrentmax = numpy.random.normal((firstattribute.maximum - firstattribute.minimum) / 2, 1)
-        if firstattributecurrentmax < firstattributecurrentmin:
-            temp = firstattributecurrentmin
-            firstattributecurrentmin = firstattributecurrentmax
-            firstattributecurrentmax = temp
-        if attributenr == 2:
-            secondattributecurrentmin = numpy.random.normal((secondattribute.maximum - secondattribute.minimum) / 2, 1)
-            secondattributecurrentmax = numpy.random.normal((secondattribute.maximum - secondattribute.minimum) / 2, 1)
-            if secondattributecurrentmax < secondattributecurrentmin:
-                temp = secondattributecurrentmin
-                secondattributecurrentmin = secondattributecurrentmax
-                secondattributecurrentmax = temp
-        # building the querry
-        querryremote = 'SELECT COUNT(*) FROM jan08 WHERE ' + firstattribute.name + ' BETWEEN ' + str(firstattributecurrentmin) + ' AND ' + str(firstattributecurrentmax)
-        if attributenr == 2:
-            querryremote = querryremote + ' AND ' + secondattribute.name + ' BETWEEN ' + str( secondattributecurrentmin) + ' AND ' + str(secondattributecurrentmax)
-        querrylocal = 'SELECT id FROM jan08 WHERE ' + firstattribute.name + ' BETWEEN ' + str(firstattributecurrentmin) + ' AND ' + str(firstattributecurrentmax)
-        if attributenr == 2:
-            querrylocal = querrylocal + ' AND ' + secondattribute.name + ' BETWEEN ' + str(secondattributecurrentmin) + ' AND ' + str(secondattributecurrentmax)
-        # querrying local db and filling the matrix
-        cur.execute(querrylocal)
-        currentrow = []
-        while True:
-            row = cur.fetchone()
-            if row == None:
-                break
-            currentrow.append(row)
-        matrix.append(currentrow)
-        print(i)
-        print(matrix)
-         # querrying remote db and filling the vector
-
-
-   # cur.execute('SELECT MAX(total_amount) FROM jan08')
-   # one = cur.fetchall()
-   # print(one)
-
-except psycopg2.DatabaseError as e:
-    print('Error %s' % e)
-    sys.exit(1)
-
-finally:
-
-    if con:
-        con.close()
-
+  # Solve the system.
+  solver.Solve()
+  opt_solution = 3 * x.solution_value() + 4 * y.solution_value()
+  print('Number of variables =', solver.NumVariables())
+  print('Number of constraints =', solver.NumConstraints())
+  # The value of each variable in the solution.
+  print('Solution:')
+  print('x = ', x.solution_value())
+  print('y = ', y.solution_value())
+  # The objective value of the solution.
+  print('Optimal objective value =', opt_solution)
+if __name__ == '__main__':
+  main()
